65c832d09eeb461c5cb0c735e5c56da6a5883c9d
mork <mfinerty1@hotmail.com>
2024-05-16 15:50:46 -0400

    delete useless files

--- src/components/MySpinner.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export function MySpinner() {
-  return (
-    <div className="flex flex-wrap items-center gap-2">
-      <Spinner aria-label="Extra large spinner example" size="xl" />
-    </div>
-  );
-}
