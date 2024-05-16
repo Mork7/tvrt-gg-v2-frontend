8e2fb2b2c9c5a6a8dc60400897d58e944ae5f306
mork <mfinerty1@hotmail.com>
2024-05-16 15:50:05 -0400

    add max retries to leagueApi

--- src/utils/leagueApi.js
+++ src/utils/leagueApi.js
@@ -1,30 +1,41 @@
 // api.js
-import axios from "axios";
+import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
+const MAX_RETRIES = 3;
 
 const api = axios.create({
-  baseURL: "https://league-of-legends-galore.p.rapidapi.com",
-  headers: {
-    "X-RapidAPI-Key": API_KEY,
-    "X-RapidAPI-Host": "league-of-legends-galore.p.rapidapi.com",
-  },
+    baseURL: 'https://league-of-legends-galore.p.rapidapi.com',
+    headers: {
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': 'league-of-legends-galore.p.rapidapi.com',
+    },
 });
 
-export const getPlayerRank = async (summonerName, tagLine, region) => {
-  try {
-    const response = await api.get("/api/getPlayerRank", {
-      method: "GET",
-      params: {
-        name: `${summonerName}-${tagLine}`,
-        region: region,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching player rank:", error);
-    throw error;
-  }
+export const getPlayerRank = async (
+    summonerName,
+    tagLine,
+    region,
+    retries = 0
+) => {
+    try {
+        const response = await api.get('/api/getPlayerRank', {
+            method: 'GET',
+            params: {
+                name: `${summonerName}-${tagLine}`,
+                region: region,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching player rank:', error);
+
+        if (retries < MAX_RETRIES) {
+            return getPlayerRank(summonerName, tagLine, region, retries + 1);
+        } else {
+            throw error;
+        }
+    }
 };
 
 // export const getChampion = async (championName) => {
@@ -41,26 +52,38 @@ export const getPlayerRank = async (summonerName, tagLine, region) => {
 //   }
 // };
 
-export const getTwitchStreams = async () => {
-  try {
-    const response = await api.get("/api/getListOfStreams", {
-      params: {
-        numOfClips: "6",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching streams:", error);
-    throw error;
-  }
+export const getTwitchStreams = async (retries = 0) => {
+    try {
+        const response = await api.get('/api/getListOfStreams', {
+            params: {
+                numOfClips: '6',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching streams:', error);
+
+        if (retries < MAX_RETRIES) {
+            console.log(`Retrying... (${retries + 1})`);
+            return getTwitchStreams(retries + 1);
+        } else {
+            throw error;
+        }
+    }
 };
 
-export const getAllLeagueRanks = async () => {
-  try {
-    const response = await api.get("/api/getLoLRanks");
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching ranks:", error);
-    throw error;
-  }
-}
+export const getAllLeagueRanks = async (retries = 0) => {
+    try {
+        const response = await api.get('/api/getLoLRanks');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching ranks:', error);
+
+        if (retries < MAX_RETRIES) {
+            console.log(`Retrying... (${retries + 1})`);
+            return getTwitchStreams(retries + 1);
+        } else {
+            throw error;
+        }
+    }
+};
